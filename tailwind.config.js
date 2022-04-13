fbdce64d441f03cefd4a1f427ec5dba0da705097
KoyWangdu <franfra10j@gmail.com>
2022-04-14 02:23:28 +0800

    tailwind_practice_v1.0

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      primary: '#0070f3',
+      secondary: '#ff4081'
+    },
+    fontFamily: {
+      sans: ['Poppins','sans-serif','Arial','Helvetica','sans-serif'],
+  },
+  extend: {},
+},
+  plugins: [],
+}
